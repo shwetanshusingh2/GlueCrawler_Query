4aaefc08d5abd02619c76b71a3ada4779d350c81
shwetanshusingh2 <32520526+shwetanshusingh2@users.noreply.github.com>
2019-10-18 12:26:33 +0530

    Update Crawler_Status.py

--- Crawler_Status.py
+++ Crawler_Status.py
@@ -5,8 +5,9 @@ class Check_Crawler():
     def crawler_status(self):
         response = user_variable.GLUE_CLIENT.get_crawler(Name='CSVCrawler')
         return response['Crawler']['State']
-
-
+     
+    # it check if the crawler is working and waits till it is in ready state
+    
     def run_crawler(self):
         print("crawler started")
         time.sleep(6)
